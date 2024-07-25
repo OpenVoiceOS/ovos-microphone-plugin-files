9eea4d7ecfa8b73018f9e310ecdffeeac7b67421
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-07-25 05:02:38 +0100

    fix entrypoint

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def required(requirements_file):
                 if pkg.strip() and not pkg.startswith("#")]
 
 
-PLUGIN_ENTRY_POINT = 'ovos-microphone-plugin-files = ovos_microphone_plugin_files:filesMicrophone'
+PLUGIN_ENTRY_POINT = 'ovos-microphone-plugin-files = ovos_microphone_plugin_files:FilesMicrophone'
 
 
 setup(
